d60a1d17c1c6a2046c36eb0436340774aef0390c
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-07-26 17:16:02 -0300

    added Info component

---
+++ src/components/InfoComponent.tsx
@@ -0,0 +1,24 @@
+import { Grid } from "@mui/material";
+import { LastAccess } from "./access/LastAccess";
+import { LastCoupons } from "./coupons/LastCoupons";
+import { MedicCertificate } from "./MedicCertificate/MedicCertificate";
+import { HistoryPayments } from "./payments/HistoryPayments";
+
+export const InfoComponent = () => {
+  return (
+    <Grid container sx={{ mt: 4 }} spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <LastAccess />
+        <LastCoupons />{" "}
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+        <HistoryPayments />
+      </Grid>
+
+      <Grid item xs={12}>
+        <MedicCertificate />
+      </Grid>
+    </Grid>
+  );
+};
