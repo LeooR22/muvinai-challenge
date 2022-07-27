98a62b1a5bdb7ddfbfdf4addb4b578d624ba5a65
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-07-27 15:07:45 -0300

    modified structure

--- src/components/InfoComponent.tsx
+++ src/components/InfoComponent.tsx
@@ -1,12 +1,17 @@
 import { Grid } from "@mui/material";
 import { LastAccess } from "./access/LastAccess";
 import { LastCoupons } from "./coupons/LastCoupons";
+import { EditionHistorial } from "./editionHistorial/EditionHistorial";
 import { MedicCertificate } from "./MedicCertificate/MedicCertificate";
 import { HistoryPayments } from "./payments/HistoryPayments";
 
 export const InfoComponent = () => {
   return (
     <Grid container sx={{ mt: 4 }} spacing={2}>
+      <Grid item xs={12}>
+        <MedicCertificate />
+      </Grid>
+
       <Grid item xs={12} sm={6}>
         <LastAccess />
         <LastCoupons />{" "}
@@ -17,7 +22,7 @@ export const InfoComponent = () => {
       </Grid>
 
       <Grid item xs={12}>
-        <MedicCertificate />
+        <EditionHistorial />
       </Grid>
     </Grid>
   );
