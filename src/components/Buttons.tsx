e195f98a5740b1085727698180952950d610af14
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-07-27 15:36:09 -0300

    added functions in buttons

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,35 +1,89 @@
-import { Box, Button, Grid } from "@mui/material";
+import { Box, Button, Grid, Link } from "@mui/material";
+import { useContext } from "react";
+import { UserContext } from "../context/user";
 
 export const Buttons = () => {
+  const { userForm } = useContext(UserContext);
+
+  const { telephone, email } = userForm;
+
+  const handleUnsubscribe = () => {
+    console.log(`Dar baja usuario ${email}`);
+  };
+
+  const handleInvalidate = () => {
+    console.log(`Invalidar usuario ${email}`);
+  };
+
+  const handleSave = () => {
+    console.log(`Guardar usuario `);
+  };
+
+  const handleSubscribe = () => {
+    console.log(`dar alta usuario ${email}`);
+  };
+
+  const sendWhatsapp = () => {};
+
   return (
     <Box display="flex" justifyContent="center" sx={{ mt: 4 }}>
       <Grid container spacing={2}>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" size="large" fullWidth color="error">
+          <Button
+            variant="outlined"
+            size="large"
+            fullWidth
+            color="error"
+            onClick={handleUnsubscribe}
+          >
             Dar baja
           </Button>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" size="large" fullWidth color="warning">
+          <Button
+            variant="outlined"
+            size="large"
+            fullWidth
+            color="warning"
+            onClick={handleInvalidate}
+          >
             Invalidar
           </Button>{" "}
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" size="large" fullWidth>
+          <Button
+            variant="outlined"
+            size="large"
+            fullWidth
+            onClick={handleSave}
+          >
             Guardar
           </Button>{" "}
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" size="large" fullWidth color="success">
+          <Button
+            variant="outlined"
+            size="large"
+            fullWidth
+            color="success"
+            onClick={handleSubscribe}
+          >
             {" "}
             Dar alta
           </Button>{" "}
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" size="large" fullWidth color="success">
-            {" "}
-            Enviar WhatsApp
-          </Button>{" "}
+          <Link
+            href={`https://wa.me/+54${telephone}`}
+            target="_blank"
+            rel="noopener"
+            underline="none"
+            color="inherit"
+          >
+            <Button variant="outlined" size="large" fullWidth color="success">
+              Enviar WhatsApp
+            </Button>{" "}
+          </Link>
         </Grid>
         <Grid item xs={6} sm={3}>
           <Button variant="outlined" size="large" fullWidth color="secondary">
@@ -38,10 +92,18 @@ export const Buttons = () => {
           </Button>{" "}
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" size="large" fullWidth>
-            {" "}
-            Enviar Email
-          </Button>{" "}
+          <Link
+            href={`mailto:${email}`}
+            target="_blank"
+            rel="noopener"
+            underline="none"
+            color="inherit"
+          >
+            <Button variant="outlined" size="large" fullWidth>
+              {" "}
+              Enviar Email
+            </Button>{" "}
+          </Link>
         </Grid>
       </Grid>
     </Box>
