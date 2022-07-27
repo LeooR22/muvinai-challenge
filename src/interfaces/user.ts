803dacf790efea67a37cdc249337777d899045d9
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-07-27 14:14:31 -0300

    interfaces of user created

---
+++ src/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface UserForm {
+  firstName: string;
+  lastName: string;
+  plan: string;
+  dni: string;
+  telephone: string;
+  alta: string;
+  email: string;
+  bornDate: string;
+}
